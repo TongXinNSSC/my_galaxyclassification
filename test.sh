68213aa7cd5cdbf6e61b5fae147e6f8de52b2e4c
TongXinNSSC <38885550+TongXinNSSC@users.noreply.github.com>
2018-07-09 15:37:43 +0800

    Add files via upload

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 python -u MLP.py --evaluate \
 --train-root '.' \
 --val-root '.' \
---batch-size  128 \
+--batch-size 128 \
 --test-batch-size 100 \
 --epochs 500 \
 --lr 0.01 \
@@ -11,10 +11,10 @@ python -u MLP.py --evaluate \
 --decay_epoch 140 \
 --decay_rate 0.2 \
 --multi_gpu \
---save-path 'checkpoint/' \
+--no-cuda \
+--save-path 'checkpointtest/' \
 --load-path 'checkpoint/_204.pth.tar' \
  > log_test.txt
 #--resume \
 #--resume \
-#--no-cuda \
 #--tsne \
